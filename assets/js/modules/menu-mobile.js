d4dd76b4d6f6788a0001994d6aa90c29e5094b2e
rdsmatheus <matheus_rds1998@hotmail.com>
2023-03-04 22:53:53 -0300

    resolvido erro do logo no iphone

--- assets/js/modules/menu-mobile.js
+++ assets/js/modules/menu-mobile.js
@@ -1,8 +1,7 @@
 export default function initMenuMobile(){
   const btn = document.querySelector("[data-menu='abrir']");
 
-  function handleTouch(event) {
-    event.target.preventDefault();
+  function handleTouch() {
     const menu = document.querySelector(".menu");
     console.log(menu);
     btn.classList.toggle("ativo");
