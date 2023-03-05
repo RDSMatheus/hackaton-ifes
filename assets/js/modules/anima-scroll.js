c9bee1f24161889be7750a3f47f24e99651f685f
rdsmatheus <matheus_rds1998@hotmail.com>
2023-03-05 19:09:17 -0300

    update

--- assets/js/modules/anima-scroll.js
+++ assets/js/modules/anima-scroll.js
@@ -1,8 +1,9 @@
 export default function initAnimaScroll() {
   const sections = document.querySelectorAll("[data-show]");
+  sections[0].classList.add("ativo");
   console.log(sections);
   function animaScroll() {
-    const distanciaWindow = window.innerHeight * 0.6;
+    const distanciaWindow = window.innerHeight * 0.9;
     sections.forEach((section) => {
       const sectionDistancia = section.getBoundingClientRect();
       const visibleScroll = sectionDistancia.top - distanciaWindow < 0;
